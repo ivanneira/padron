e92eafd61a4b11450a79cf4ce266c7e227b92253
Iván Neira <ien1983@gmail.com>
2018-10-08 10:30:51 -0300

    exportar sin marca

--- routes/exportar.js
+++ routes/exportar.js
@@ -13,9 +13,8 @@ router.get('/excel', function(req, res, next) {
     //console.log(1)
 
     var workbook = new excel.Workbook();
-    var wsPlanillas = workbook.addWorksheet('planillas');
-    var wsEncuestas = workbook.addWorksheet('encuestas');
-    var wsRegistros = workbook.addWorksheet('registros');
+    var wsPlanillas = workbook.addWorksheet('visitados');
+    var wsEncuestas = workbook.addWorksheet('sin visitar');
 
     var estiloFecha = workbook.createStyle({
         numberFormat: 'd/m/yy'
@@ -170,8 +169,73 @@ router.get('/excel', function(req, res, next) {
         wsPlanillas.column(25).setWidth(20);
         wsPlanillas.column(26).setWidth(30);
 
-        workbook.write('listado.xlsx',res);
+        knex.raw("select Dni,  Sexo, Ejemplar, Vencimiento, Emision, Apellido, Nombre, Nacimiento, Fallecimiento, Cuil, Calle, Piso, Departamento, Cpostal,  Barrio, Monoblock, Ciudad, Municipio, Provincia, Pais  from padron pa where pa.id not in (select re.registro from registro re)")
+        .then(function(row2,e2){
 
+            if(e2){
+                //console.log(e)
+                res.send(false)
+              }else if(row2){
+
+
+                
+                wsEncuestas.cell(1,1).string("Dni").style(estiloBold);
+                wsEncuestas.cell(1,2).string("Sexo").style(estiloBold);
+                wsEncuestas.cell(1,3).string("Ejemplar").style(estiloBold);
+                wsEncuestas.cell(1,4).string("Vencimiento").style(estiloBold);
+                wsEncuestas.cell(1,5).string("Emision").style(estiloBold);
+                wsEncuestas.cell(1,6).string("Apellido").style(estiloBold);
+                wsEncuestas.cell(1,7).string("Nombre").style(estiloBold);
+                wsEncuestas.cell(1,8).string("Nacimiento").style(estiloBold);
+                wsEncuestas.cell(1,9).string("Fallecimiento").style(estiloBold);
+                wsEncuestas.cell(1,10).string("Cuil").style(estiloBold);
+                wsEncuestas.cell(1,11).string("Calle").style(estiloBold);
+                wsEncuestas.cell(1,12).string("Piso").style(estiloBold);
+                wsEncuestas.cell(1,13).string("Departamento").style(estiloBold);
+                wsEncuestas.cell(1,14).string("Cpostal").style(estiloBold);
+                wsEncuestas.cell(1,15).string("Barrio").style(estiloBold);
+                wsEncuestas.cell(1,16).string("Monoblock").style(estiloBold);
+                wsEncuestas.cell(1,17).string("Ciudad").style(estiloBold);
+                wsEncuestas.cell(1,18).string("Municipio").style(estiloBold);
+                wsEncuestas.cell(1,19).string("Provincia").style(estiloBold);
+                wsEncuestas.cell(1,20).string("Pais").style(estiloBold);
+
+                
+                var filecounter = 2;
+
+                for(var index in row2){
+
+                    wsPlanillas.cell(filecounter,1).string(row2[index].Dni).style(estiloCentrado);
+                    wsPlanillas.cell(filecounter,2).string(row2[index].Sexo).style(estiloCentrado);
+                    wsPlanillas.cell(filecounter,3).string(row2[index].Ejemplar).style(estiloCentrado);
+                    wsPlanillas.cell(filecounter,4).date(new Date(row2[index].Vencimiento)).style(estiloCentrado).style(estiloFecha);
+                    wsPlanillas.cell(filecounter,5).date(new Date(row2[index].Emision)).style(estiloCentrado).style(estiloFecha);
+                    wsPlanillas.cell(filecounter,6).string(row2[index].Apellido);
+                    wsPlanillas.cell(filecounter,7).string(row2[index].Nombre).style(estiloCentrado);
+                    wsPlanillas.cell(filecounter,8).date(new Date(row2[index].Nacimiento)).style(estiloCentrado).style(estiloFecha);
+                    wsPlanillas.cell(filecounter,9).date(new Date(row2[index].Fallecimiento)).style(estiloCentrado).style(estiloFecha);
+                    wsPlanillas.cell(filecounter,10).string(row2[index].Cuil);
+                    wsPlanillas.cell(filecounter,11).string(row2[index].Calle);
+                    wsPlanillas.cell(filecounter,12).string(row2[index].Piso).style(estiloCentrado).style(estiloFecha);
+                    wsPlanillas.cell(filecounter,13).string(row2[index].Departamento.toString()).style(estiloCentrado);
+                    wsPlanillas.cell(filecounter,14).string(row2[index].Cpostal);
+                    wsPlanillas.cell(filecounter,15).string(row2[index].Barrio);
+                    wsPlanillas.cell(filecounter,16).string(row2[index].Monoblock);
+                    wsPlanillas.cell(filecounter,17).string(row2[index].Ciudad);
+                    wsPlanillas.cell(filecounter,18).string(row2[index].Municipio);
+                    wsPlanillas.cell(filecounter,19).string(row2[index].Provincia);
+                    wsPlanillas.cell(filecounter,20).string(row2[index].Pais);
+
+                    filecounter++;
+                }
+
+              }
+
+            workbook.write('listado.xlsx',res);
+
+            
+        });
+        
       }else{
         res.send(false)
       }
